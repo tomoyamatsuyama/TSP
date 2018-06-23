771aa7697a8cd4cb54c7d5744b5d6a0d05cf4abd
tomoyamatsuyama <m.tomoya0221@gmail.com>
2018-06-23 20:12:14 +0900

    Dist, 3628

--- reidai01/Solver.java
+++ reidai01/Solver.java
@@ -7,26 +7,82 @@ import java.util.*;
 public class Solver {
 
 	public static List<Integer> positions = new ArrayList<>();
+	public static List<Integer> nearList1 = new ArrayList<>();
+	public static List<Integer> nearList2 = new ArrayList<>();
+	public static List<Integer> nearList3 = new ArrayList<>();
+
+	public static int threePickedUp(int dis, int first, List<Integer> currentPositions) {
+		currentPositions.add(first);
+		List<Integer> positionList = new ArrayList<>(positions);
+		positionList.removeAll(currentPositions);
+		int min = 500;
+		int nextPosition = 0;
+		for (Integer position: positionList) {
+			int distance = TSP2D.distance(first, position.intValue());
+			if (distance != 0) {
+				if (min >= distance) {
+					min = distance;
+					nextPosition = position.intValue();
+				}
+			} 
+		}
+		currentPositions.add(nextPosition);
+		positionList.removeAll(currentPositions);
+
+		for (Integer position: positionList) {
+			int distance = TSP2D.distance(nextPosition, position.intValue());
+			if (distance != 0) {
+				if (min >= distance) {
+					min += distance;
+					nextPosition = position.intValue();
+				}
+			} 
+		}
+		return min + dis;
+	}
 
 	public static int nearst(List<Integer> currentPosition) {
+		// 近いやつ3つをピックアップする。
 		Integer current = currentPosition.get(currentPosition.size()-1);
 		List<Integer> positionList = new ArrayList<>(positions);
+		HashMap<Integer, Integer> map = new HashMap<>();
 
 		positionList.removeAll(currentPosition);
 		int min = 500;
 		int nextPosition = 0;
 		for (Integer position: positionList) {
 			int distance = TSP2D.distance(current.intValue(), position.intValue());
-			if (min >= distance) {
-				min = distance;
-				nextPosition = position.intValue();
+			if (distance != 0) {
+				Integer distan = Integer.valueOf(distance);
+				map.put(distan, position);
 			}
+		}
+
+		Object[] mapkey = map.keySet().toArray();
+		Arrays.sort(mapkey);
+		int count = 0;
+		int threeDistance = 50000;
+		for (Integer key: map.keySet()) {
+			if (count == 3) {
+				break;
+			}
+			// System.out.println(key);
+			int posi = map.get(key).intValue();
+			int threeDis = threePickedUp(key, posi, positionList);
+
+			if (threeDistance > threeDis) {
+				System.out.println(key);
+				nextPosition = posi;
+				threeDistance = threeDis;
+			}
+			count++;
 		}
 
 		return nextPosition;
 	}
 
 	public static void set(int range) {
+		// for initilize 
 		for(int i = 0; i < range; i++) {
 			positions.add(i, i);
 		}
@@ -40,7 +96,9 @@ public class Solver {
 		currentPatern.add(0);
 
 		for (int i = 1; i < num; i++) {
-			currentPatern.add(nearst(currentPatern));
+			int posi = nearst(currentPatern);
+			System.out.println(posi);
+			currentPatern.add(posi);
 		}
 
 		Integer[] order = new Integer[num-1];
